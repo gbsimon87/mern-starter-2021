2f5584b7622cb3c3deb477bccb6899b4a13de54d
Simon Cordova <>
2021-01-17 20:47:34 +0000

    Add MongoDB/Mongoose, env files, and Readme

--- src/server/app.js
+++ src/server/app.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+require("dotenv").config();
 
 const indexRouter = require("./routes/index");
-const app = express();
 const BUILD_PATH = path.join(__dirname, "../../", "build");
+const uri = process.env.DB_HOST;
 
+const app = express();
 app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
@@ -18,6 +21,13 @@ app.use(express.static(BUILD_PATH, { index: false }));
 app.use("/api", indexRouter);
 app.use("/*", (req, res, next) => {
   res.sendFile(path.join(BUILD_PATH, "index.html"));
+});
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
 });
 
 module.exports = app;
